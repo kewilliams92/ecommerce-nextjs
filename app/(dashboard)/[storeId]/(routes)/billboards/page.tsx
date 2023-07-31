d31787a85eb19e46da09a4eaeae98450037386f4
kw92 <kewilliams2017@hotmail.com>
2023-07-31 14:38:21 -0400

    creation of billboards is done.

---
+++ app/(dashboard)/[storeId]/(routes)/billboards/page.tsx
@@ -0,0 +1,13 @@
+import { BillboardClient } from "./components/client"
+
+const BillboardsPage = () => {
+    return (
+        <div>
+            <div>
+                <BillboardClient />
+            </div>
+        </div>
+    )
+    }
+
+export default BillboardsPage
--- app/api/stores/[storeId]/deleteRoute.ts
+++
@@ -1,3 +0,0 @@
-import prismadb from "@/lib/prismadb";
-import { auth } from "@clerk/nextjs"
-import { NextResponse } from "next/server"
